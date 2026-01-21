e01168b179112c53322a9549c6d00126e2811f24
Douglas_Michelini <douglasmsrodrigues@hotmail.com>
2026-01-21 13:45:54 -0300

    Removendo o formulário de contato

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -11,7 +11,6 @@ const Navbar = () => {
   const menuItems = [
     { label: "Sobre", href: "/" },
     { label: "Portfólio", href: "/portfolio" },
-    { label: "Contato", href: "/contact" },
   ];
 
   return (
