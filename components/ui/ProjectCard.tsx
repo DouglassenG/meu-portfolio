c79bc79eddd085121ae006a2286642e682a79412
Douglas_Michelini <douglasmsrodrigues@hotmail.com>
2026-02-13 13:45:36 -0300

    Atualizando os cards

--- components/ui/ProjectCard.tsx
+++ components/ui/ProjectCard.tsx
@@ -24,7 +24,7 @@ const ProjectCard = ({
   siteUrl,
 }: ProjectCardProps) => {
   return (
-    <div className="flex flex-col rounded-lg overflow-hidden shadow-md border-2 border-transparent bg-card text-card-foreground p-4 sm:p-6">
+    <div className="flex flex-col rounded-lg overflow-hidden shadow-md border-2 border-transparent bg-card text-card-foreground p-4 sm:p-6 transition-all duration-300 hover:brightness-90">
       <h3 className="text-lg sm:text-xl font-bold mb-4 text-center">{title}</h3>
       <div className="relative w-full h-48 sm:h-64 mb-4">
         <img
@@ -33,19 +33,25 @@ const ProjectCard = ({
           className="object-cover w-full h-full rounded-md"
         />
       </div>
-      <div className="flex-grow">
-        <h4 className="font-semibold mt-4 text-base sm:text-lg">
-          Objetivo do Projeto
-        </h4>
-        <p className="text-sm text-muted-foreground mb-4">{objetivo}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 flex-grow mb-4">
+        {/* Right column: Objective and Result */}
+        <div className="md:col-start-2">
+          <h4 className="font-semibold text-base sm:text-lg">
+            Objetivo do Projeto
+          </h4>
+          <p className="text-sm text-muted-foreground mb-4">{objetivo}</p>
 
-        <h4 className="font-semibold text-base sm:text-lg">
-          Tecnologia Utilizada
-        </h4>
-        <p className="text-sm text-muted-foreground mb-4">{tecnologia}</p>
+          <h4 className="font-semibold text-base sm:text-lg">Resultado</h4>
+          <p className="text-sm text-muted-foreground">{resultado}</p>
+        </div>
 
-        <h4 className="font-semibold text-base sm:text-lg">Resultado</h4>
-        <p className="text-sm text-muted-foreground mb-4">{resultado}</p>
+        {/* Left column: Technologies */}
+        <div className="md:col-start-1 md:row-start-1">
+          <h4 className="font-semibold text-base sm:text-lg">
+            Tecnologia Utilizada
+          </h4>
+          <p className="text-sm text-muted-foreground">{tecnologia}</p>
+        </div>
       </div>
 
       <div className="flex justify-center space-x-4 mt-auto">
